2ff27980a8e71bbb54c5b6c4e12fbb0c80c6111a
Stephen Dickinson <stephencottontail@me.com>
2020-09-27 09:24:53 -0600

    Add now page

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,6 @@ export default function Home( { data } ) {
 	)
 }
 
-
 /**
  * `formatString` is created using momentjs tokens
  *
---
+++ src/pages/now.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default function Now( { data } ) {
+	const { title, content, modified } = data.wpPage
+
+	return (
+		<article>
+			<header>
+				<h1>{ title }</h1>
+				<p>{ modified }</p>
+			</header>
+			<div
+				dangerouslySetInnerHTML={{ __html: content }}
+			/>
+		</article>
+	)
+}
+
+export const pageQuery = graphql`
+query {
+	wpPage(slug: { eq: "now" }) {
+		title
+		content
+		modified(formatString: "Do")
+	}
+}
+`
