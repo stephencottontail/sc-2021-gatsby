d18f5a5d235269a724a09f97c54f0428bf2839f3
Stephen Dickinson <stephencottontail@me.com>
2020-10-01 19:17:16 -0600

    Static front page + archive for plain blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,48 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+import classnames from 'classnames';
 import Layout from '../components/Layout'
-import Categories from '../components/Categories';
-import Tags from '../components/Tags';
 
 export default function Home( { data } ) {
+	const post = data.allWpPage.edges[0].node;
+
 	return (
 		<Layout>
+			<header
+				className={ classnames( 'page-header' ) }
+			>
+				<h1
+					className={ classnames( 'page-title' ) }
+				>
+					Steve
+					<span>Web Designer/Developer</span>
+					<span>Contributor to the GitHub Arctic Code Vault*</span>
+				</h1>
+				<p>*No, really. <a href="https://github.com/stephencottontail/">Check for yourself.</a></p>
+			</header>
 			<main>
-				{ data.allWpPost.edges.map( ( { node } ) => {
-					return (
-						<article
-							key={ node.databaseId }
-							id={ node.databaseId }
-						>
-							<header>
-								<Link to={ `blog/${node.slug}` }>
-									<h2>{ node.title }</h2>
-								</Link>
-								<p>
-									<span><b>Date</b>{ node.date }</span>
-									{ node.categories && <Categories src={ node.categories } /> }
-									{ node.tags && <Tags src={ node.tags } /> }
-								</p>
-							</header>
-						</article>
-					)
-				} ) }
+				<div
+					dangerouslySetInnerHTML={{ __html: post.content }}
+				/>
 			</main>
 		</Layout>
 	)
 }
 
 /**
- * `formatString` is created using momentjs tokens
+ * for now, it looks like you have to hardcode in the
+ * ID if you're using a static front page
  *
- * @link https://momentjs.com/docs/#/displaying/format/
+ * @link https://github.com/gatsbyjs/gatsby-source-wordpress-experimental/issues/194
  */
 export const pageQuery = graphql`
 query {
-	allWpPost( sort: { fields: [date] } ) {
+	allWpPage( filter: { databaseId: { eq: 44 } } ) {
 		edges {
 			node {
-				title
-				excerpt
-				databaseId
-				slug
-				date( formatString: "MMMM D YYYY" )
-				categories {
-					nodes {
-						name
-					}
-				}
-				tags {
-					nodes {
-						name
-					}
-				}
+				content
 			}
 		}
 	}
 }
 `
-
